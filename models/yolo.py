4ddf97cc0cbf4f8d396000ab1fb41b5dfa3001e6
cjsy18114 <2901515639@qq.com>
2026-02-27 10:56:40 +0800

    Update yolo.py

--- models/yolo.py
+++ models/yolo.py
@@ -115,7 +115,7 @@ class Model(nn.Module):
         if isinstance(m, Detect):
             s = 256  # 2x min stride
             m.inplace = self.inplace
-            m.stride = torch.tensor([s / x.shape[-2] for x in self.forward(torch.zeros(1,time_window, ch, s, s))])  # forward
+            m.stride = torch.tensor([s / x.shape[-2] for x in self.forward(torch.zeros(1, ch, s, s))]) # forward
             m.anchors /= m.stride.view(-1, 1, 1)
             check_anchor_order(m)
             self.stride = m.stride
